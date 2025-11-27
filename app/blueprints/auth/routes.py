46bdf9d8416f940a48d68a309eb888fa2a69a7df
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-11-27 13:51:13 +0800

    feat: update login functionality and styles for improved user experience

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,7 +1,7 @@
 # app/blueprints/auth/routes.py
 from datetime import timedelta
 
-from flask import redirect, render_template, request, url_for
+from flask import make_response, redirect, render_template, request, url_for
 from flask_login import login_required, login_user, logout_user
 from sqlalchemy import or_
 
@@ -23,11 +23,10 @@ def login():
             if user and user.check_password(form.password.data):
                 login_user(user, remember=form.remember.data, duration=timedelta(days=1))
 
-                """
                 if request.headers.get("HX-Request"):
                     response = make_response("", 204)
-                    response.headers["HX-Redirect"] = url_for("tracker.view_vouchers")
-                    return response """
+                    response.headers["HX-Redirect"] = url_for("index")
+                    return response
 
             if not user:
                 form.identifier.errors.append("Couldn't find that email or username")

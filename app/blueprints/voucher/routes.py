e9b6a8b71f87cb5bc655d82e946d8c2a293b7eae
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-11-29 03:46:29 +0100

    feat: add total vouchers count and enhance voucher detail view with pagination

--- app/blueprints/voucher/routes.py
+++ app/blueprints/voucher/routes.py
@@ -11,6 +11,24 @@ def all_vouchers():
     per_page = 25
 
     pagination = DisbursementVoucher.query.paginate(page=page, per_page=per_page, error_out=False)
+    total_vouchers = DisbursementVoucher.query.count()
 
     template = "vouchers_partials.html" if request.headers.get("HX-Request") else "vouchers.html"
-    return render_template(template, pagination=pagination)
+    return render_template(template, pagination=pagination, total_vouchers=total_vouchers)
+
+
+@voucher_bp.route("/<int:voucher_id>")
+def view_voucher(voucher_id):
+    voucher_number = request.args.get("voucher_number")  # Fetch the voucher_number
+
+    voucher = DisbursementVoucher.query.get(voucher_id)
+    page = request.args.get("page", 1, type=int)
+    per_page = 25
+
+    pagination = DisbursementVoucher.query.paginate(page=page, per_page=per_page, error_out=False)
+    total_vouchers = DisbursementVoucher.query.count()
+
+    template = "voucher_partials.html" if request.headers.get("HX-Request") else "voucher.html"
+    return render_template(
+        template, voucher=voucher, pagination=pagination, total_vouchers=total_vouchers, voucher_number=voucher_number
+    )
